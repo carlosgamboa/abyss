481422b71dbd0c34499bd077ce1db4d8b49eb98e
Carlos Gamboa <cgamboav41@gmail.com>
2019-04-08 13:40:39 -0600

    linker options updated

--- setAMPIConfig.sh
+++ setAMPIConfig.sh
@@ -4,7 +4,10 @@
 	--with-mpi=/opt/tools/openmpi-3.1.2-gcc-7.2.0/bin/mpirun \
 	CPPFLAGS=-I/opt/tools/sparsehash-git/include \
 	CXXFLAGS='-tlsglobals' \
+	LDFLAGS='-tlsglobals -tracemode projections' \
 	--with-boost=/home/cgamboa/tools/local/boost_1_69_0 \
-	--prefix=/home/cgamboa/paroba/ampi-o
+	--prefix=/home/cgamboa/paroba/ampi-trcp
+
+# -tracemode projections
 
 
